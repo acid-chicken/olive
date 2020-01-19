3a0480d5255dba692cd74ce60c1441f87a73c107
Restyled.io <commits@restyled.io>
2020-01-19 11:55:39 +0000

    Restyled by shfmt

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,19 +2,19 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-    # Qt 5.10.1 (may have to source newer version at some point)
-    sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-trusty -y
+  # Qt 5.10.1 (may have to source newer version at some point)
+  sudo add-apt-repository ppa:beineri/opt-qt-5.10.1-trusty -y
 
-    # FFmpeg 4.x
-    sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y
+  # FFmpeg 4.x
+  sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y
 
-    # OpenImageIO
-    sudo add-apt-repository ppa:olive-editor/openimageio -y
+  # OpenImageIO
+  sudo add-apt-repository ppa:olive-editor/openimageio -y
 
-    # OpenColorIO
-    sudo add-apt-repository ppa:olive-editor/opencolorio -y
+  # OpenColorIO
+  sudo add-apt-repository ppa:olive-editor/opencolorio -y
 
-    # Update apt
-    sudo apt-get update -qq
+  # Update apt
+  sudo apt-get update -qq
 
 fi
