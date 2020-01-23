6a483273e8f1235a48aa1ff62203ee109e5d9309
Restyled.io <commits@restyled.io>
2020-01-23 14:55:59 +0000

    Restyled by astyle

--- app/dialog/rendercancel/rendercancel.cpp
+++ app/dialog/rendercancel/rendercancel.cpp
@@ -1,58 +1,58 @@
 #include "rendercancel.h"
 
 RenderCancelDialog::RenderCancelDialog(QWidget *parent) :
-  LoadSaveDialog(tr("Waiting for workers to finish..."), tr("Renderer"), parent),
-  busy_workers_(0),
-  total_workers_(0)
+    LoadSaveDialog(tr("Waiting for workers to finish..."), tr("Renderer"), parent),
+    busy_workers_(0),
+    total_workers_(0)
 {
 }
 
 void RenderCancelDialog::RunIfWorkersAreBusy()
 {
-  if (busy_workers_ > 0) {
-    waiting_workers_ = busy_workers_;
+    if (busy_workers_ > 0) {
+        waiting_workers_ = busy_workers_;
 
-    exec();
-  }
+        exec();
+    }
 }
 
 void RenderCancelDialog::SetWorkerCount(int count)
 {
-  total_workers_ = count;
+    total_workers_ = count;
 
-  UpdateProgress();
+    UpdateProgress();
 }
 
 void RenderCancelDialog::WorkerStarted()
 {
-  busy_workers_++;
+    busy_workers_++;
 
-  UpdateProgress();
+    UpdateProgress();
 }
 
 void RenderCancelDialog::WorkerDone()
 {
-  busy_workers_--;
+    busy_workers_--;
 
-  UpdateProgress();
+    UpdateProgress();
 }
 
 void RenderCancelDialog::showEvent(QShowEvent *event)
 {
-  QDialog::showEvent(event);
-  
-  UpdateProgress();
+    QDialog::showEvent(event);
+
+    UpdateProgress();
 }
 
 void RenderCancelDialog::UpdateProgress()
 {
-  if (!total_workers_ || !isVisible()) {
-    return;
-  }
+    if (!total_workers_ || !isVisible()) {
+        return;
+    }
 
-  SetProgress(qRound(100.0 * static_cast<double>(waiting_workers_ - busy_workers_) / static_cast<double>(waiting_workers_)));
+    SetProgress(qRound(100.0 * static_cast<double>(waiting_workers_ - busy_workers_) / static_cast<double>(waiting_workers_)));
 
-  if (busy_workers_ == 0) {
-    accept();
-  }
+    if (busy_workers_ == 0) {
+        accept();
+    }
 }
