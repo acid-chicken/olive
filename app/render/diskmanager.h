935944a40258a1009eda933cda48dde4e156a344
Restyled.io <commits@restyled.io>
2020-01-25 02:55:45 +0000

    Restyled by clang-format

--- app/render/diskmanager.h
+++ app/render/diskmanager.h
@@ -4,53 +4,51 @@
 #include <QMutex>
 #include <QObject>
 
-class DiskManager : public QObject
-{
-    Q_OBJECT
-public:
-    static void CreateInstance();
+class DiskManager : public QObject {
+  Q_OBJECT
+ public:
+  static void CreateInstance();
 
-    static void DestroyInstance();
+  static void DestroyInstance();
 
-    static DiskManager* instance();
+  static DiskManager* instance();
 
-    void Accessed(const QByteArray& hash);
+  void Accessed(const QByteArray& hash);
 
-    void Accessed(const QString& filename);
+  void Accessed(const QString& filename);
 
-    void CreatedFile(const QString& file_name, const QByteArray& hash);
+  void CreatedFile(const QString& file_name, const QByteArray& hash);
 
-    bool ClearDiskCache(bool quick_delete);
+  bool ClearDiskCache(bool quick_delete);
 
-signals:
-    void DeletedFrame(const QByteArray& hash);
+ signals:
+  void DeletedFrame(const QByteArray& hash);
 
-private:
-    DiskManager();
+ private:
+  DiskManager();
 
-    virtual ~DiskManager() override;
+  virtual ~DiskManager() override;
 
-    static DiskManager* instance_;
+  static DiskManager* instance_;
 
-    QByteArray DeleteLeastRecent();
+  QByteArray DeleteLeastRecent();
 
-    qint64 DiskLimit();
+  qint64 DiskLimit();
 
-    static QString GetCacheIndexFilename();
+  static QString GetCacheIndexFilename();
 
-    struct HashTime {
-        QString file_name;
-        QByteArray hash;
-        qint64 access_time;
-        qint64 file_size;
-    };
+  struct HashTime {
+    QString file_name;
+    QByteArray hash;
+    qint64 access_time;
+    qint64 file_size;
+  };
 
-    QList<HashTime> disk_data_;
+  QList<HashTime> disk_data_;
 
-    qint64 consumption_;
+  qint64 consumption_;
 
-    QMutex lock_;
-
+  QMutex lock_;
 };
 
-#endif // DISKMANAGER_H
+#endif  // DISKMANAGER_H
