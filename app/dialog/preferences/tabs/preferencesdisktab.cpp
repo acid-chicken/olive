46e7e7840cf5d1a576276ed5b497f1586dc63e3b
Restyled.io <commits@restyled.io>
2020-01-25 02:55:44 +0000

    Restyled by astyle

--- app/dialog/preferences/tabs/preferencesdisktab.cpp
+++ app/dialog/preferences/tabs/preferencesdisktab.cpp
@@ -12,116 +12,116 @@
 
 PreferencesDiskTab::PreferencesDiskTab()
 {
-  QVBoxLayout* outer_layout = new QVBoxLayout(this);
+    QVBoxLayout* outer_layout = new QVBoxLayout(this);
 
-  QGroupBox* disk_management_group = new QGroupBox(tr("Disk Management"));
-  outer_layout->addWidget(disk_management_group);
+    QGroupBox* disk_management_group = new QGroupBox(tr("Disk Management"));
+    outer_layout->addWidget(disk_management_group);
 
-  QGridLayout* disk_management_layout = new QGridLayout(disk_management_group);
+    QGridLayout* disk_management_layout = new QGridLayout(disk_management_group);
 
-  int row = 0;
+    int row = 0;
 
-  disk_management_layout->addWidget(new QLabel(tr("Disk Cache Location:")), row, 0);
+    disk_management_layout->addWidget(new QLabel(tr("Disk Cache Location:")), row, 0);
 
-  disk_cache_location_ = new QLineEdit();
-  disk_cache_location_->setText(Config::Current()["DiskCachePath"].toString());
-  connect(disk_cache_location_, &QLineEdit::textChanged, this, &PreferencesDiskTab::DiskCacheLineEditChanged);
-  disk_management_layout->addWidget(disk_cache_location_, row, 1);
+    disk_cache_location_ = new QLineEdit();
+    disk_cache_location_->setText(Config::Current()["DiskCachePath"].toString());
+    connect(disk_cache_location_, &QLineEdit::textChanged, this, &PreferencesDiskTab::DiskCacheLineEditChanged);
+    disk_management_layout->addWidget(disk_cache_location_, row, 1);
 
-  QPushButton* browse_btn = new QPushButton(tr("Browse"));
-  connect(browse_btn, &QPushButton::clicked, this, &PreferencesDiskTab::BrowseDiskCachePath);
-  disk_management_layout->addWidget(browse_btn, row, 2);
+    QPushButton* browse_btn = new QPushButton(tr("Browse"));
+    connect(browse_btn, &QPushButton::clicked, this, &PreferencesDiskTab::BrowseDiskCachePath);
+    disk_management_layout->addWidget(browse_btn, row, 2);
 
-  row++;
+    row++;
 
-  disk_management_layout->addWidget(new QLabel(tr("Maximum Disk Cache:")), row, 0);
+    disk_management_layout->addWidget(new QLabel(tr("Maximum Disk Cache:")), row, 0);
 
-  maximum_cache_slider_ = new FloatSlider();
-  maximum_cache_slider_->SetSuffix(QStringLiteral(" GB"));
-  maximum_cache_slider_->SetMinimum(1.0);
-  maximum_cache_slider_->SetValue(Config::Current()["DiskCacheSize"].toDouble());
-  disk_management_layout->addWidget(maximum_cache_slider_, row, 1, 1, 2);
+    maximum_cache_slider_ = new FloatSlider();
+    maximum_cache_slider_->SetSuffix(QStringLiteral(" GB"));
+    maximum_cache_slider_->SetMinimum(1.0);
+    maximum_cache_slider_->SetValue(Config::Current()["DiskCacheSize"].toDouble());
+    disk_management_layout->addWidget(maximum_cache_slider_, row, 1, 1, 2);
 
-  row++;
+    row++;
 
-  QPushButton* clear_cache_btn = new QPushButton(tr("Clear Disk Cache"));
-  connect(clear_cache_btn, &QPushButton::clicked, this, &PreferencesDiskTab::ClearDiskCache);
-  disk_management_layout->addWidget(clear_cache_btn, row, 1, 1, 2);
+    QPushButton* clear_cache_btn = new QPushButton(tr("Clear Disk Cache"));
+    connect(clear_cache_btn, &QPushButton::clicked, this, &PreferencesDiskTab::ClearDiskCache);
+    disk_management_layout->addWidget(clear_cache_btn, row, 1, 1, 2);
 
-  row++;
+    row++;
 
-  clear_disk_cache_ = new QCheckBox(tr("Automatically clear disk cache on close"));
-  clear_disk_cache_->setChecked(Config::Current()["ClearDiskCacheOnClose"].toBool());
-  disk_management_layout->addWidget(clear_disk_cache_, row, 1, 1, 2);
+    clear_disk_cache_ = new QCheckBox(tr("Automatically clear disk cache on close"));
+    clear_disk_cache_->setChecked(Config::Current()["ClearDiskCacheOnClose"].toBool());
+    disk_management_layout->addWidget(clear_disk_cache_, row, 1, 1, 2);
 
-  QGroupBox* cache_behavior = new QGroupBox(tr("Cache Behavior"));
-  outer_layout->addWidget(cache_behavior);
-  QGridLayout* cache_behavior_layout = new QGridLayout(cache_behavior);
+    QGroupBox* cache_behavior = new QGroupBox(tr("Cache Behavior"));
+    outer_layout->addWidget(cache_behavior);
+    QGridLayout* cache_behavior_layout = new QGridLayout(cache_behavior);
 
-  row = 0;
+    row = 0;
 
-  cache_behavior_layout->addWidget(new QLabel(tr("Cache Ahead:")), row, 0);
+    cache_behavior_layout->addWidget(new QLabel(tr("Cache Ahead:")), row, 0);
 
-  cache_ahead_slider_ = new FloatSlider();
-  cache_ahead_slider_->SetSuffix(QStringLiteral(" seconds"));
-  cache_ahead_slider_->SetValue(Config::Current()["DiskCacheAhead"].value<rational>().toDouble());
-  cache_behavior_layout->addWidget(cache_ahead_slider_, row, 1);
+    cache_ahead_slider_ = new FloatSlider();
+    cache_ahead_slider_->SetSuffix(QStringLiteral(" seconds"));
+    cache_ahead_slider_->SetValue(Config::Current()["DiskCacheAhead"].value<rational>().toDouble());
+    cache_behavior_layout->addWidget(cache_ahead_slider_, row, 1);
 
-  cache_behavior_layout->addWidget(new QLabel(tr("Cache Behind:")), row, 2);
+    cache_behavior_layout->addWidget(new QLabel(tr("Cache Behind:")), row, 2);
 
-  cache_behind_slider_ = new FloatSlider();
-  cache_behind_slider_->SetSuffix(QStringLiteral(" seconds"));
-  cache_behind_slider_->SetValue(Config::Current()["DiskCacheBehind"].value<rational>().toDouble());
-  cache_behavior_layout->addWidget(cache_behind_slider_, row, 3);
+    cache_behind_slider_ = new FloatSlider();
+    cache_behind_slider_->SetSuffix(QStringLiteral(" seconds"));
+    cache_behind_slider_->SetValue(Config::Current()["DiskCacheBehind"].value<rational>().toDouble());
+    cache_behavior_layout->addWidget(cache_behind_slider_, row, 3);
 
-  outer_layout->addStretch();
+    outer_layout->addStretch();
 }
 
 void PreferencesDiskTab::Accept()
 {
-  Config::Current()["DiskCachePath"] = disk_cache_location_->text();
-  Config::Current()["DiskCacheSize"] = maximum_cache_slider_->GetValue();
-  Config::Current()["ClearDiskCacheOnClose"] = clear_disk_cache_->isChecked();
-  Config::Current()["DiskCacheBehind"] = QVariant::fromValue(rational::fromDouble(cache_behind_slider_->GetValue()));
-  Config::Current()["DiskCacheAhead"] = QVariant::fromValue(rational::fromDouble(cache_ahead_slider_->GetValue()));
+    Config::Current()["DiskCachePath"] = disk_cache_location_->text();
+    Config::Current()["DiskCacheSize"] = maximum_cache_slider_->GetValue();
+    Config::Current()["ClearDiskCacheOnClose"] = clear_disk_cache_->isChecked();
+    Config::Current()["DiskCacheBehind"] = QVariant::fromValue(rational::fromDouble(cache_behind_slider_->GetValue()));
+    Config::Current()["DiskCacheAhead"] = QVariant::fromValue(rational::fromDouble(cache_ahead_slider_->GetValue()));
 }
 
 void PreferencesDiskTab::DiskCacheLineEditChanged()
 {
-  QString entered_dir = disk_cache_location_->text();
+    QString entered_dir = disk_cache_location_->text();
 
-  if (!entered_dir.isEmpty() && !QDir(entered_dir).exists()) {
-    disk_cache_location_->setStyleSheet(QStringLiteral("color: red;"));
-  } else {
-    disk_cache_location_->setStyleSheet(QString());
-  }
+    if (!entered_dir.isEmpty() && !QDir(entered_dir).exists()) {
+        disk_cache_location_->setStyleSheet(QStringLiteral("color: red;"));
+    } else {
+        disk_cache_location_->setStyleSheet(QString());
+    }
 }
 
 void PreferencesDiskTab::BrowseDiskCachePath()
 {
-  QString dir = QFileDialog::getExistingDirectory(this);
+    QString dir = QFileDialog::getExistingDirectory(this);
 
-  if (!dir.isEmpty()) {
-    disk_cache_location_->setText(dir);
-  }
+    if (!dir.isEmpty()) {
+        disk_cache_location_->setText(dir);
+    }
 }
 
 void PreferencesDiskTab::ClearDiskCache()
 {
-  if (QMessageBox::question(this,
-                            tr("Clear Disk Cache"),
-                            tr("Are you sure you want to clear the disk cache in '%1'?").arg(Config::Current()["DiskCachePath"].toString()),
-                            QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
-    if (DiskManager::instance()->ClearDiskCache(false)) {
-      QMessageBox::information(this,
-                               tr("Clear Disk Cache"),
-                               tr("Disk cache cleared successfully"),
-                               QMessageBox::Ok);
-    } else {
-      QMessageBox::information(this,
-                               tr("Clear Disk Cache"),
-                               tr("Disk cache failed to fully clear. You may have to delete the cache files manually."),
-                               QMessageBox::Ok);
+    if (QMessageBox::question(this,
+                              tr("Clear Disk Cache"),
+                              tr("Are you sure you want to clear the disk cache in '%1'?").arg(Config::Current()["DiskCachePath"].toString()),
+                              QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes) {
+        if (DiskManager::instance()->ClearDiskCache(false)) {
+            QMessageBox::information(this,
+                                     tr("Clear Disk Cache"),
+                                     tr("Disk cache cleared successfully"),
+                                     QMessageBox::Ok);
+        } else {
+            QMessageBox::information(this,
+                                     tr("Clear Disk Cache"),
+                                     tr("Disk cache failed to fully clear. You may have to delete the cache files manually."),
+                                     QMessageBox::Ok);
+        }
     }
-  }
 }
--- app/render/diskmanager.h
+++ app/render/diskmanager.h
@@ -6,50 +6,50 @@
 
 class DiskManager : public QObject
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  static void CreateInstance();
+    static void CreateInstance();
 
-  static void DestroyInstance();
+    static void DestroyInstance();
 
-  static DiskManager* instance();
+    static DiskManager* instance();
 
-  void Accessed(const QByteArray& hash);
+    void Accessed(const QByteArray& hash);
 
-  void Accessed(const QString& filename);
+    void Accessed(const QString& filename);
 
-  void CreatedFile(const QString& file_name, const QByteArray& hash);
+    void CreatedFile(const QString& file_name, const QByteArray& hash);
 
-  bool ClearDiskCache(bool quick_delete);
+    bool ClearDiskCache(bool quick_delete);
 
 signals:
-  void DeletedFrame(const QByteArray& hash);
+    void DeletedFrame(const QByteArray& hash);
 
 private:
-  DiskManager();
+    DiskManager();
 
-  virtual ~DiskManager() override;
+    virtual ~DiskManager() override;
 
-  static DiskManager* instance_;
+    static DiskManager* instance_;
 
-  QByteArray DeleteLeastRecent();
+    QByteArray DeleteLeastRecent();
 
-  qint64 DiskLimit();
+    qint64 DiskLimit();
 
-  static QString GetCacheIndexFilename();
+    static QString GetCacheIndexFilename();
 
-  struct HashTime {
-    QString file_name;
-    QByteArray hash;
-    qint64 access_time;
-    qint64 file_size;
-  };
+    struct HashTime {
+        QString file_name;
+        QByteArray hash;
+        qint64 access_time;
+        qint64 file_size;
+    };
 
-  QList<HashTime> disk_data_;
+    QList<HashTime> disk_data_;
 
-  qint64 consumption_;
+    qint64 consumption_;
 
-  QMutex lock_;
+    QMutex lock_;
 
 };
 
