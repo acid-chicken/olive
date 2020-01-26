f4b1572e87289b1a44b518d0bd6fd3f1ca9372fb
Restyled.io <commits@restyled.io>
2020-01-26 01:55:52 +0000

    Restyled by shfmt

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,11 +2,11 @@
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
-    export PATH="/usr/local/opt/qt/bin:/usr/local/opt/python@2/libexec/bin:$PATH"
+  export PATH="/usr/local/opt/qt/bin:/usr/local/opt/python@2/libexec/bin:$PATH"
 
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-    sudo apt-get -y -o Dpkg::Options::="--force-overwrite" install qt511base qt511multimedia qt511svg qt511tools libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libswscale-dev libswresample-dev cmake libopencolorio-dev libopenimageio-dev libgl1-mesa-dev
-    source /opt/qt*/bin/qt*-env.sh
+  sudo apt-get -y -o Dpkg::Options::="--force-overwrite" install qt511base qt511multimedia qt511svg qt511tools libavformat-dev libavcodec-dev libavfilter-dev libavutil-dev libswscale-dev libswresample-dev cmake libopencolorio-dev libopenimageio-dev libgl1-mesa-dev
+  source /opt/qt*/bin/qt*-env.sh
 
 fi
