2065a4b67f95a770b6b5512e0f2f36c854af3684
Restyled.io <commits@restyled.io>
2020-02-21 15:48:48 +0000

    Restyled by astyle

--- app/render/backend/opengl/openglcolorprocessor.cpp
+++ app/render/backend/opengl/openglcolorprocessor.cpp
@@ -7,71 +7,71 @@
 
 void OpenGLColorProcessor::Enable(QOpenGLContext *context, bool alpha_is_associated)
 {
-  if (IsEnabled()) {
-    return;
-  }
+    if (IsEnabled()) {
+        return;
+    }
 
-  context_ = context;
+    context_ = context;
 
-  pipeline_ = OpenGLShader::CreateOCIO(context_,
-                                       ocio_lut_,
-                                       GetProcessor(),
-                                       alpha_is_associated);
+    pipeline_ = OpenGLShader::CreateOCIO(context_,
+                                         ocio_lut_,
+                                         GetProcessor(),
+                                         alpha_is_associated);
 
-  connect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture, Qt::DirectConnection);
+    connect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture, Qt::DirectConnection);
 }
 
 bool OpenGLColorProcessor::IsEnabled() const
 {
-  return ocio_lut_;
+    return ocio_lut_;
 }
 
 OpenGLShaderPtr OpenGLColorProcessor::pipeline() const
 {
-  return pipeline_;
+    return pipeline_;
 }
 
 void OpenGLColorProcessor::ProcessOpenGL(bool flipped, const QMatrix4x4& matrix)
 {
-  OpenGLRenderFunctions::OCIOBlit(pipeline_, ocio_lut_, flipped, matrix);
+    OpenGLRenderFunctions::OCIOBlit(pipeline_, ocio_lut_, flipped, matrix);
 }
 
 void OpenGLColorProcessor::ClearTexture()
 {
-  if (IsEnabled()) {
-    // Clean up OCIO LUT texture and shader
-    context_->functions()->glDeleteTextures(1, &ocio_lut_);
+    if (IsEnabled()) {
+        // Clean up OCIO LUT texture and shader
+        context_->functions()->glDeleteTextures(1, &ocio_lut_);
 
-    disconnect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture);
+        disconnect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture);
 
-    ocio_lut_ = 0;
-    pipeline_ = nullptr;
-  }
+        ocio_lut_ = 0;
+        pipeline_ = nullptr;
+    }
 }
 
 OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &dest_space) :
-  ColorProcessor(config, source_space, dest_space),
-  ocio_lut_(0)
+    ColorProcessor(config, source_space, dest_space),
+    ocio_lut_(0)
 {
 }
 
 OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space, QString display, QString view, const QString &look) :
-  ColorProcessor(config, source_space, display, view, look),
-  ocio_lut_(0)
+    ColorProcessor(config, source_space, display, view, look),
+    ocio_lut_(0)
 {
 }
 
 OpenGLColorProcessor::~OpenGLColorProcessor()
 {
-  ClearTexture();
+    ClearTexture();
 }
 
 OpenGLColorProcessorPtr OpenGLColorProcessor::Create(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &dest_space)
 {
-  return std::make_shared<OpenGLColorProcessor>(config, source_space, dest_space);
+    return std::make_shared<OpenGLColorProcessor>(config, source_space, dest_space);
 }
 
 OpenGLColorProcessorPtr OpenGLColorProcessor::Create(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &display, const QString &view, const QString &look)
 {
-  return std::make_shared<OpenGLColorProcessor>(config, source_space, display, view, look);
+    return std::make_shared<OpenGLColorProcessor>(config, source_space, display, view, look);
 }
--- app/widget/timelinewidget/undo/undo.h
+++ app/widget/timelinewidget/undo/undo.h
@@ -30,97 +30,97 @@
 
 class BlockResizeCommand : public QUndoCommand {
 public:
-  BlockResizeCommand(Block* block, rational new_length, QUndoCommand* parent = nullptr);
+    BlockResizeCommand(Block* block, rational new_length, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  Block* block_;
-  rational old_length_;
-  rational new_length_;
+    Block* block_;
+    rational old_length_;
+    rational new_length_;
 };
 
 class BlockResizeWithMediaInCommand : public QUndoCommand {
 public:
-  BlockResizeWithMediaInCommand(Block* block, rational new_length, QUndoCommand* parent = nullptr);
+    BlockResizeWithMediaInCommand(Block* block, rational new_length, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  Block* block_;
-  rational old_length_;
-  rational new_length_;
+    Block* block_;
+    rational old_length_;
+    rational new_length_;
 };
 
 class BlockSetMediaInCommand : public QUndoCommand {
 public:
-  BlockSetMediaInCommand(Block* block, rational new_media_in, QUndoCommand* parent = nullptr);
+    BlockSetMediaInCommand(Block* block, rational new_media_in, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  Block* block_;
-  rational old_media_in_;
-  rational new_media_in_;
+    Block* block_;
+    rational old_media_in_;
+    rational new_media_in_;
 };
 
 class BlockSetSpeedCommand : public QUndoCommand {
 public:
-  BlockSetSpeedCommand(Block* block, const rational& new_speed, QUndoCommand* parent = nullptr);
+    BlockSetSpeedCommand(Block* block, const rational& new_speed, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  Block* block_;
+    Block* block_;
 
-  rational old_speed_;
-  rational new_speed_;
+    rational old_speed_;
+    rational new_speed_;
 };
 
 class TrackRippleRemoveBlockCommand : public QUndoCommand {
 public:
-  TrackRippleRemoveBlockCommand(TrackOutput* track, Block* block, QUndoCommand* parent = nullptr);
+    TrackRippleRemoveBlockCommand(TrackOutput* track, Block* block, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  TrackOutput* track_;
+    TrackOutput* track_;
 
-  Block* block_;
+    Block* block_;
 
-  Block* before_;
+    Block* before_;
 };
 
 class TrackPrependBlockCommand : public QUndoCommand {
 public:
-  TrackPrependBlockCommand(TrackOutput* track, Block* block, QUndoCommand* parent = nullptr);
+    TrackPrependBlockCommand(TrackOutput* track, Block* block, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  TrackOutput* track_;
-  Block* block_;
+    TrackOutput* track_;
+    Block* block_;
 };
 
 class TrackInsertBlockAfterCommand : public QUndoCommand {
 public:
-  TrackInsertBlockAfterCommand(TrackOutput* track, Block* block, Block* before, QUndoCommand* parent = nullptr);
+    TrackInsertBlockAfterCommand(TrackOutput* track, Block* block, Block* before, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  TrackOutput* track_;
+    TrackOutput* track_;
 
-  Block* block_;
+    Block* block_;
 
-  Block* before_;
+    Block* before_;
 };
 
 /**
@@ -132,33 +132,33 @@ private:
  */
 class TrackRippleRemoveAreaCommand : public QUndoCommand {
 public:
-  TrackRippleRemoveAreaCommand(TrackOutput* track, rational in, rational out, QUndoCommand* parent = nullptr);
+    TrackRippleRemoveAreaCommand(TrackOutput* track, rational in, rational out, QUndoCommand* parent = nullptr);
 
-  void SetInsert(Block* insert);
+    void SetInsert(Block* insert);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 protected:
-  TrackOutput* track_;
-  rational in_;
-  rational out_;
+    TrackOutput* track_;
+    rational in_;
+    rational out_;
 
-  bool splice_;
+    bool splice_;
 
-  Block* trim_out_;
-  Block* trim_in_;
-  QVector<Block*> removed_blocks_;
+    Block* trim_out_;
+    Block* trim_in_;
+    QVector<Block*> removed_blocks_;
 
-  rational trim_in_old_length_;
-  rational trim_out_old_length_;
+    rational trim_in_old_length_;
+    rational trim_out_old_length_;
 
-  rational trim_in_new_length_;
-  rational trim_out_new_length_;
+    rational trim_in_new_length_;
+    rational trim_out_new_length_;
 
-  Block* insert_;
+    Block* insert_;
 
-  QObject memory_manager_;
+    QObject memory_manager_;
 };
 
 /**
@@ -170,57 +170,57 @@ protected:
  */
 class TrackPlaceBlockCommand : public TrackRippleRemoveAreaCommand {
 public:
-  TrackPlaceBlockCommand(TrackList *timeline, int track, Block* block, rational in, QUndoCommand* parent = nullptr);
+    TrackPlaceBlockCommand(TrackList *timeline, int track, Block* block, rational in, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  TrackList* timeline_;
-  int track_index_;
-  bool append_;
-  GapBlock* gap_;
-  int added_track_count_;
+    TrackList* timeline_;
+    int track_index_;
+    bool append_;
+    GapBlock* gap_;
+    int added_track_count_;
 };
 
 class BlockSplitCommand : public QUndoCommand {
 public:
-  BlockSplitCommand(TrackOutput* track, Block* block, rational point, QUndoCommand* parent = nullptr);
+    BlockSplitCommand(TrackOutput* track, Block* block, rational point, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
-  Block* new_block();
+    Block* new_block();
 
 private:
-  TrackOutput* track_;
-  Block* block_;
+    TrackOutput* track_;
+    Block* block_;
 
-  rational new_length_;
-  rational old_length_;
-  rational point_;
+    rational new_length_;
+    rational old_length_;
+    rational point_;
 
-  Block* new_block_;
+    Block* new_block_;
 
-  QList<NodeInput*> transitions_to_move_;
+    QList<NodeInput*> transitions_to_move_;
 
-  QObject memory_manager_;
+    QObject memory_manager_;
 
 };
 
 class TrackSplitAtTimeCommand : public QUndoCommand {
 public:
-  TrackSplitAtTimeCommand(TrackOutput* track, rational point, QUndoCommand* parent = nullptr);
+    TrackSplitAtTimeCommand(TrackOutput* track, rational point, QUndoCommand* parent = nullptr);
 };
 
 class BlockSplitPreservingLinksCommand : public QUndoCommand {
 public:
-  BlockSplitPreservingLinksCommand(const QVector<Block *> &blocks, const QList<rational>& times, QUndoCommand* parent = nullptr);
+    BlockSplitPreservingLinksCommand(const QVector<Block *> &blocks, const QList<rational>& times, QUndoCommand* parent = nullptr);
 
 private:
-  QVector<Block *> blocks_;
+    QVector<Block *> blocks_;
 
-  QList<rational> times_;
+    QList<rational> times_;
 };
 
 /**
@@ -230,40 +230,40 @@ private:
  */
 class TrackReplaceBlockCommand : public QUndoCommand {
 public:
-  TrackReplaceBlockCommand(TrackOutput* track, Block* old, Block* replace, QUndoCommand* parent = nullptr);
+    TrackReplaceBlockCommand(TrackOutput* track, Block* old, Block* replace, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  TrackOutput* track_;
-  Block* old_;
-  Block* replace_;
+    TrackOutput* track_;
+    Block* old_;
+    Block* replace_;
 };
 
 class TrackCleanGapsCommand : public QUndoCommand {
 public:
-  TrackCleanGapsCommand(TrackList* track_list, int index, QUndoCommand* parent = nullptr);
+    TrackCleanGapsCommand(TrackList* track_list, int index, QUndoCommand* parent = nullptr);
 
-  virtual void redo() override;
-  virtual void undo() override;
+    virtual void redo() override;
+    virtual void undo() override;
 
 private:
-  struct MergedGap {
-    GapBlock* merged;
-    rational original_length;
-    QList<GapBlock*> removed;
-  };
+    struct MergedGap {
+        GapBlock* merged;
+        rational original_length;
+        QList<GapBlock*> removed;
+    };
 
-  TrackList* track_list_;
+    TrackList* track_list_;
 
-  int track_index_;
+    int track_index_;
 
-  QObject memory_manager_;
+    QObject memory_manager_;
 
-  QList<MergedGap> merged_gaps_;
+    QList<MergedGap> merged_gaps_;
 
-  QList<GapBlock*> removed_end_gaps_;
+    QList<GapBlock*> removed_end_gaps_;
 
 };
 
