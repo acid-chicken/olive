b792c032ebcf0a609c4c84bc4e51726542e5affc
Restyled.io <commits@restyled.io>
2020-02-21 09:48:47 +0000

    Restyled by astyle

--- app/codec/decoder.cpp
+++ app/codec/decoder.cpp
@@ -34,14 +34,14 @@
 #include "task/taskmanager.h"
 
 Decoder::Decoder() :
-  open_(false),
-  stream_(nullptr)
+    open_(false),
+    stream_(nullptr)
 {
 }
 
 Decoder::Decoder(Stream *fs) :
-  open_(false),
-  stream_(fs)
+    open_(false),
+    stream_(fs)
 {
 }
 
@@ -51,34 +51,34 @@ Decoder::~Decoder()
 
 StreamPtr Decoder::stream()
 {
-  return stream_;
+    return stream_;
 }
 
 void Decoder::set_stream(StreamPtr fs)
 {
-  Close();
+    Close();
 
-  stream_ = fs;
+    stream_ = fs;
 }
 
 FramePtr Decoder::RetrieveVideo(const rational &/*timecode*/)
 {
-  return nullptr;
+    return nullptr;
 }
 
 FramePtr Decoder::RetrieveAudio(const rational &/*timecode*/, const rational &/*length*/, const AudioRenderingParams &/*params*/)
 {
-  return nullptr;
+    return nullptr;
 }
 
 bool Decoder::SupportsVideo()
 {
-  return false;
+    return false;
 }
 
 bool Decoder::SupportsAudio()
 {
-  return false;
+    return false;
 }
 
 /*
@@ -86,239 +86,239 @@ bool Decoder::SupportsAudio()
  */
 
 QVector<DecoderPtr> ReceiveListOfAllDecoders() {
-  QVector<DecoderPtr> decoders;
+    QVector<DecoderPtr> decoders;
 
-  // The order in which these decoders are added is their priority when probing. Hence FFmpeg should usually be last,
-  // since it supports so many formats and we presumably want to override those formats with a more specific decoder.
+    // The order in which these decoders are added is their priority when probing. Hence FFmpeg should usually be last,
+    // since it supports so many formats and we presumably want to override those formats with a more specific decoder.
 
-  decoders.append(std::make_shared<OIIODecoder>());
-  decoders.append(std::make_shared<FFmpegDecoder>());
+    decoders.append(std::make_shared<OIIODecoder>());
+    decoders.append(std::make_shared<FFmpegDecoder>());
 
-  return decoders;
+    return decoders;
 }
 
 bool Decoder::ProbeMedia(Footage *f, const QAtomicInt* cancelled)
 {
-  // Check for a valid filename
-  if (f->filename().isEmpty()) {
-    qWarning() << "Tried to probe media with an empty filename";
-    return false;
-  }
-
-  // Check file exists
-  if (!QFileInfo::exists(f->filename())) {
-    qWarning() << "Tried to probe file that doesn't exist:" << f->filename();
-    return false;
-  }
-
-  // Reset Footage state for probing
-  f->Clear();
-
-  // Create list to iterate through
-  QVector<DecoderPtr> decoder_list = ReceiveListOfAllDecoders();
-
-  // Pass Footage through each Decoder's probe function
-  for (int i=0;i<decoder_list.size();i++) {
-
-    if (cancelled && *cancelled) {
-      return false;
+    // Check for a valid filename
+    if (f->filename().isEmpty()) {
+        qWarning() << "Tried to probe media with an empty filename";
+        return false;
     }
 
-    DecoderPtr decoder = decoder_list.at(i);
+    // Check file exists
+    if (!QFileInfo::exists(f->filename())) {
+        qWarning() << "Tried to probe file that doesn't exist:" << f->filename();
+        return false;
+    }
 
-    if (decoder->Probe(f, cancelled)) {
+    // Reset Footage state for probing
+    f->Clear();
 
-      // We found a Decoder, so we can set this media as valid
-      f->set_status(Footage::kReady);
+    // Create list to iterate through
+    QVector<DecoderPtr> decoder_list = ReceiveListOfAllDecoders();
 
-      // Attach the successful Decoder to this Footage object
-      f->set_decoder(decoder->id());
+    // Pass Footage through each Decoder's probe function
+    for (int i=0; i<decoder_list.size(); i++) {
 
-      // FIXME: Cache the results so we don't have to probe if this media is added a second time
-
-      // Start an index task
-      foreach (StreamPtr stream, f->streams()) {
-        if (stream->type() == Stream::kAudio) {
-          QMetaObject::invokeMethod(IndexManager::instance(),
-                                    "StartIndexingStream",
-                                    Qt::QueuedConnection,
-                                    Q_ARG(StreamPtr, stream));
+        if (cancelled && *cancelled) {
+            return false;
         }
-      }
 
-      return true;
+        DecoderPtr decoder = decoder_list.at(i);
+
+        if (decoder->Probe(f, cancelled)) {
+
+            // We found a Decoder, so we can set this media as valid
+            f->set_status(Footage::kReady);
+
+            // Attach the successful Decoder to this Footage object
+            f->set_decoder(decoder->id());
+
+            // FIXME: Cache the results so we don't have to probe if this media is added a second time
+
+            // Start an index task
+            foreach (StreamPtr stream, f->streams()) {
+                if (stream->type() == Stream::kAudio) {
+                    QMetaObject::invokeMethod(IndexManager::instance(),
+                                              "StartIndexingStream",
+                                              Qt::QueuedConnection,
+                                              Q_ARG(StreamPtr, stream));
+                }
+            }
+
+            return true;
+        }
     }
-  }
 
-  // We aren't able to use this Footage
-  f->set_status(Footage::kInvalid);
-  f->set_decoder(QString());
+    // We aren't able to use this Footage
+    f->set_status(Footage::kInvalid);
+    f->set_decoder(QString());
 
-  return false;
+    return false;
 }
 
 DecoderPtr Decoder::CreateFromID(const QString &id)
 {
-  if (id.isEmpty()) {
-    return nullptr;
-  }
-
-  // Create list to iterate through
-  QVector<DecoderPtr> decoder_list = ReceiveListOfAllDecoders();
-
-  foreach (DecoderPtr d, decoder_list) {
-    if (d->id() == id) {
-      return d;
+    if (id.isEmpty()) {
+        return nullptr;
     }
-  }
 
-  return nullptr;
+    // Create list to iterate through
+    QVector<DecoderPtr> decoder_list = ReceiveListOfAllDecoders();
+
+    foreach (DecoderPtr d, decoder_list) {
+        if (d->id() == id) {
+            return d;
+        }
+    }
+
+    return nullptr;
 }
 
 void Decoder::Conform(const AudioRenderingParams &params, const QAtomicInt* cancelled)
 {
-  if (stream()->type() != Stream::kAudio) {
-    // Nothing to be done
-    return;
-  }
-
-  // Get indexed WAV file
-  WaveInput input(GetIndexFilename());
-
-  // FIXME: No handling if input failed to open/is corrupt
-  if (input.open()) {
-    // If the parameters are equal, nothing to be done
-    // FIXME: Technically we only need to conform if the SAMPLE RATE is not equal. Format and channel layout conversion
-    //        could be done on the fly so we could perhaps conform less often at some point.
-    if (input.params() == params) {
-      input.close();
-      return;
+    if (stream()->type() != Stream::kAudio) {
+        // Nothing to be done
+        return;
     }
 
-    // Otherwise, let's start converting the format
-    QMutexLocker locker(stream()->index_process_lock());
+    // Get indexed WAV file
+    WaveInput input(GetIndexFilename());
 
-    // Generate destination filename for this conversion to see if it exists
-    QString conformed_fn = GetConformedFilename(params);
+    // FIXME: No handling if input failed to open/is corrupt
+    if (input.open()) {
+        // If the parameters are equal, nothing to be done
+        // FIXME: Technically we only need to conform if the SAMPLE RATE is not equal. Format and channel layout conversion
+        //        could be done on the fly so we could perhaps conform less often at some point.
+        if (input.params() == params) {
+            input.close();
+            return;
+        }
 
-    if (QFileInfo::exists(conformed_fn)) {
-      // We must have already conformed this format
-      std::static_pointer_cast<AudioStream>(stream())->append_conformed_version(params);
-      input.close();
-      return;
-    }
+        // Otherwise, let's start converting the format
+        QMutexLocker locker(stream()->index_process_lock());
 
-    // Set up resampler
-    SwrContext* resampler = swr_alloc_set_opts(nullptr,
-                                               static_cast<int64_t>(params.channel_layout()),
-                                               FFmpegCommon::GetFFmpegSampleFormat(params.format()),
-                                               params.sample_rate(),
-                                               static_cast<int64_t>(input.params().channel_layout()),
-                                               FFmpegCommon::GetFFmpegSampleFormat(input.params().format()),
-                                               input.params().sample_rate(),
-                                               0,
-                                               nullptr);
+        // Generate destination filename for this conversion to see if it exists
+        QString conformed_fn = GetConformedFilename(params);
 
-    swr_init(resampler);
+        if (QFileInfo::exists(conformed_fn)) {
+            // We must have already conformed this format
+            std::static_pointer_cast<AudioStream>(stream())->append_conformed_version(params);
+            input.close();
+            return;
+        }
 
-    WaveOutput conformed_output(conformed_fn, params);
-    if (!conformed_output.open()) {
-      qWarning() << "Failed to open conformed output:" << conformed_fn;
-      input.close();
-      return;
-    }
+        // Set up resampler
+        SwrContext* resampler = swr_alloc_set_opts(nullptr,
+                                static_cast<int64_t>(params.channel_layout()),
+                                FFmpegCommon::GetFFmpegSampleFormat(params.format()),
+                                params.sample_rate(),
+                                static_cast<int64_t>(input.params().channel_layout()),
+                                FFmpegCommon::GetFFmpegSampleFormat(input.params().format()),
+                                input.params().sample_rate(),
+                                0,
+                                nullptr);
 
-    // Convert one second of audio at a time
-    int input_buffer_sz = input.params().time_to_bytes(1);
+        swr_init(resampler);
 
-    int read_count = 0;
+        WaveOutput conformed_output(conformed_fn, params);
+        if (!conformed_output.open()) {
+            qWarning() << "Failed to open conformed output:" << conformed_fn;
+            input.close();
+            return;
+        }
 
-    while (!input.at_end()) {
-      if (cancelled && *cancelled) {
-        break;
-      }
+        // Convert one second of audio at a time
+        int input_buffer_sz = input.params().time_to_bytes(1);
 
-      // Read up to one second of audio from WAV file
-      QByteArray read_samples = input.read(input_buffer_sz);
+        int read_count = 0;
 
-      // Determine how many samples this is
-      int in_sample_count = input.params().bytes_to_samples(read_samples.size());
+        while (!input.at_end()) {
+            if (cancelled && *cancelled) {
+                break;
+            }
 
-      ConformInternal(resampler, &conformed_output, read_samples.data(), in_sample_count);
+            // Read up to one second of audio from WAV file
+            QByteArray read_samples = input.read(input_buffer_sz);
 
-      read_count += read_samples.size();
-      emit IndexProgress(qRound(100.0 * static_cast<double>(read_count) / static_cast<double>(input.data_length())));
-    }
+            // Determine how many samples this is
+            int in_sample_count = input.params().bytes_to_samples(read_samples.size());
 
-    // Flush resampler
-    ConformInternal(resampler, &conformed_output, nullptr, 0);
+            ConformInternal(resampler, &conformed_output, read_samples.data(), in_sample_count);
 
-    // Clean up
-    swr_free(&resampler);
-    conformed_output.close();
-    input.close();
+            read_count += read_samples.size();
+            emit IndexProgress(qRound(100.0 * static_cast<double>(read_count) / static_cast<double>(input.data_length())));
+        }
 
-    // If we cancelled, the conform didn't finish so remove it
-    if (cancelled && *cancelled) {
-      QFile(conformed_fn).remove();
+        // Flush resampler
+        ConformInternal(resampler, &conformed_output, nullptr, 0);
+
+        // Clean up
+        swr_free(&resampler);
+        conformed_output.close();
+        input.close();
+
+        // If we cancelled, the conform didn't finish so remove it
+        if (cancelled && *cancelled) {
+            QFile(conformed_fn).remove();
+        } else {
+            std::static_pointer_cast<AudioStream>(stream())->append_conformed_version(params);
+        }
     } else {
-      std::static_pointer_cast<AudioStream>(stream())->append_conformed_version(params);
+        qWarning() << "Failed to conform file:" << stream()->footage()->filename();
     }
-  } else {
-    qWarning() << "Failed to conform file:" << stream()->footage()->filename();
-  }
 }
 
 void Decoder::ConformInternal(SwrContext* resampler, WaveOutput* output, const char* in_data, int in_sample_count)
 {
-  // Determine how many samples the output will be
-  int out_sample_count = swr_get_out_samples(resampler, in_sample_count);
+    // Determine how many samples the output will be
+    int out_sample_count = swr_get_out_samples(resampler, in_sample_count);
 
-  // Allocate array for the amount of samples we'll need
-  QByteArray out_samples;
-  out_samples.resize(output->params().samples_to_bytes(out_sample_count));
+    // Allocate array for the amount of samples we'll need
+    QByteArray out_samples;
+    out_samples.resize(output->params().samples_to_bytes(out_sample_count));
 
-  char* out_data = out_samples.data();
+    char* out_data = out_samples.data();
 
-  // Convert samples
-  int convert_count = swr_convert(resampler,
-                                  reinterpret_cast<uint8_t**>(&out_data),
-                                  out_sample_count,
-                                  reinterpret_cast<const uint8_t**>(&in_data),
-                                  in_sample_count);
+    // Convert samples
+    int convert_count = swr_convert(resampler,
+                                    reinterpret_cast<uint8_t**>(&out_data),
+                                    out_sample_count,
+                                    reinterpret_cast<const uint8_t**>(&in_data),
+                                    in_sample_count);
 
-  if (convert_count != out_sample_count) {
-    out_samples.resize(output->params().samples_to_bytes(convert_count));
-  }
+    if (convert_count != out_sample_count) {
+        out_samples.resize(output->params().samples_to_bytes(convert_count));
+    }
 
-  output->write(out_samples);
+    output->write(out_samples);
 }
 
 QString Decoder::GetConformedFilename(const AudioRenderingParams &params)
 {
-  QString index_fn = GetIndexFilename();
-  WaveInput input(GetIndexFilename());
+    QString index_fn = GetIndexFilename();
+    WaveInput input(GetIndexFilename());
 
-  // FIXME: No handling if input failed to open/is corrupt
-  if (input.open()) {
-    // If the parameters are equal, nothing to be done
-    AudioRenderingParams index_params = input.params();
-    input.close();
+    // FIXME: No handling if input failed to open/is corrupt
+    if (input.open()) {
+        // If the parameters are equal, nothing to be done
+        AudioRenderingParams index_params = input.params();
+        input.close();
 
-    if (index_params == params) {
-      // Source file matches perfectly, no conform required
-      return index_fn;
+        if (index_params == params) {
+            // Source file matches perfectly, no conform required
+            return index_fn;
+        }
     }
-  }
 
-  index_fn.append('.');
-  index_fn.append(QString::number(params.sample_rate()));
-  index_fn.append('.');
-  index_fn.append(QString::number(params.format()));
-  index_fn.append('.');
-  index_fn.append(QString::number(params.channel_layout()));
+    index_fn.append('.');
+    index_fn.append(QString::number(params.sample_rate()));
+    index_fn.append('.');
+    index_fn.append(QString::number(params.format()));
+    index_fn.append('.');
+    index_fn.append(QString::number(params.channel_layout()));
 
-  return index_fn;
+    return index_fn;
 }
 
 void Decoder::Index(const QAtomicInt *)
@@ -327,33 +327,33 @@ void Decoder::Index(const QAtomicInt *)
 
 bool Decoder::HasConformedVersion(const AudioRenderingParams &params)
 {
-  if (stream()->type() != Stream::kAudio) {
-    return false;
-  }
+    if (stream()->type() != Stream::kAudio) {
+        return false;
+    }
 
-  AudioStreamPtr audio_stream = std::static_pointer_cast<AudioStream>(stream());
+    AudioStreamPtr audio_stream = std::static_pointer_cast<AudioStream>(stream());
 
-  if (audio_stream->has_conformed_version(params)) {
-    return true;
-  }
+    if (audio_stream->has_conformed_version(params)) {
+        return true;
+    }
 
-  // Get indexed WAV file
-  WaveInput input(GetIndexFilename());
+    // Get indexed WAV file
+    WaveInput input(GetIndexFilename());
 
-  bool index_already_matches = false;
+    bool index_already_matches = false;
 
-  if (input.open()) {
-    index_already_matches = (input.params() == params);
+    if (input.open()) {
+        index_already_matches = (input.params() == params);
 
-    input.close();
-  }
+        input.close();
+    }
 
-  return index_already_matches;
+    return index_already_matches;
 }
 
 void Decoder::SignalIndexProgress(const int64_t &ts)
 {
-  if (stream()->duration() != AV_NOPTS_VALUE && stream()->duration() != 0) {
-    emit IndexProgress(qRound(100.0 * static_cast<double>(ts) / static_cast<double>(stream()->duration())));
-  }
+    if (stream()->duration() != AV_NOPTS_VALUE && stream()->duration() != 0) {
+        emit IndexProgress(qRound(100.0 * static_cast<double>(ts) / static_cast<double>(stream()->duration())));
+    }
 }
--- app/codec/ffmpeg/ffmpegdecoder.h
+++ app/codec/ffmpeg/ffmpegdecoder.h
@@ -41,102 +41,102 @@ extern "C" {
  */
 class FFmpegDecoder : public Decoder
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  // Constructor
-  FFmpegDecoder();
+    // Constructor
+    FFmpegDecoder();
 
-  // Destructor
-  virtual ~FFmpegDecoder() override;
+    // Destructor
+    virtual ~FFmpegDecoder() override;
 
-  virtual bool Probe(Footage *f, const QAtomicInt *cancelled) override;
+    virtual bool Probe(Footage *f, const QAtomicInt *cancelled) override;
 
-  virtual bool Open() override;
-  virtual RetrieveState GetRetrieveState(const rational &time) override;
-  virtual FramePtr RetrieveVideo(const rational &timecode) override;
-  virtual FramePtr RetrieveAudio(const rational &timecode, const rational &length, const AudioRenderingParams& params) override;
-  virtual void Close() override;
+    virtual bool Open() override;
+    virtual RetrieveState GetRetrieveState(const rational &time) override;
+    virtual FramePtr RetrieveVideo(const rational &timecode) override;
+    virtual FramePtr RetrieveAudio(const rational &timecode, const rational &length, const AudioRenderingParams& params) override;
+    virtual void Close() override;
 
-  virtual QString id() override;
+    virtual QString id() override;
 
-  virtual bool SupportsVideo() override;
-  virtual bool SupportsAudio() override;
+    virtual bool SupportsVideo() override;
+    virtual bool SupportsAudio() override;
 
-  virtual void Index(const QAtomicInt *cancelled) override;
+    virtual void Index(const QAtomicInt *cancelled) override;
 
 signals:
-  void ConsumedMemory();
+    void ConsumedMemory();
 
 private:
-  /**
-   * @brief Handle an error
-   *
-   * Immediately closes the Decoder (freeing memory resources) and sends the string provided to the warning stream.
-   * As this function closes the Decoder, no further Decoder functions should be performed after this is called
-   * (unless the Decoder is opened again first).
-   */
-  void Error(const QString& s);
+    /**
+     * @brief Handle an error
+     *
+     * Immediately closes the Decoder (freeing memory resources) and sends the string provided to the warning stream.
+     * As this function closes the Decoder, no further Decoder functions should be performed after this is called
+     * (unless the Decoder is opened again first).
+     */
+    void Error(const QString& s);
 
-  /**
-   * @brief Handle an FFmpeg error code
-   *
-   * Uses the FFmpeg API to retrieve a descriptive string for this error code and sends it to Error(). As such, this
-   * function also automatically closes the Decoder.
-   *
-   * @param error_code
-   */
-  void FFmpegError(int error_code);
+    /**
+     * @brief Handle an FFmpeg error code
+     *
+     * Uses the FFmpeg API to retrieve a descriptive string for this error code and sends it to Error(). As such, this
+     * function also automatically closes the Decoder.
+     *
+     * @param error_code
+     */
+    void FFmpegError(int error_code);
 
-  /**
-   * @brief Uses the FFmpeg API to retrieve a packet (stored in pkt_) and decode it (stored in frame_)
-   *
-   * @return
-   *
-   * An FFmpeg error code, or >= 0 on success
-   */
-  int GetFrame(AVPacket* pkt, AVFrame* frame);
+    /**
+     * @brief Uses the FFmpeg API to retrieve a packet (stored in pkt_) and decode it (stored in frame_)
+     *
+     * @return
+     *
+     * An FFmpeg error code, or >= 0 on success
+     */
+    int GetFrame(AVPacket* pkt, AVFrame* frame);
 
-  virtual QString GetIndexFilename() override;
+    virtual QString GetIndexFilename() override;
 
-  void UnconditionalAudioIndex(const QAtomicInt* cancelled);
+    void UnconditionalAudioIndex(const QAtomicInt* cancelled);
 
-  void Seek(int64_t timestamp);
+    void Seek(int64_t timestamp);
 
-  void CacheFrameToDisk(AVFrame* f);
+    void CacheFrameToDisk(AVFrame* f);
 
-  void RemoveFirstFromFrameCache();
-  void RemoveLastFromFrameCache();
-  void ClearFrameCache();
+    void RemoveFirstFromFrameCache();
+    void RemoveLastFromFrameCache();
+    void ClearFrameCache();
 
-  void ClearResources();
+    void ClearResources();
 
-  AVFormatContext* fmt_ctx_;
-  AVCodecContext* codec_ctx_;
-  AVStream* avstream_;
+    AVFormatContext* fmt_ctx_;
+    AVCodecContext* codec_ctx_;
+    AVStream* avstream_;
 
-  AVPixelFormat ideal_pix_fmt_;
-  PixelFormat::Format native_pix_fmt_;
+    AVPixelFormat ideal_pix_fmt_;
+    PixelFormat::Format native_pix_fmt_;
 
-  SwsContext* scale_ctx_;
+    SwsContext* scale_ctx_;
 
-  QList<AVFrame*> cached_frames_;
-  bool cache_at_zero_;
-  bool cache_at_eof_;
+    QList<AVFrame*> cached_frames_;
+    bool cache_at_zero_;
+    bool cache_at_eof_;
 
-  int64_t second_ts_;
+    int64_t second_ts_;
 
-  AVDictionary* opts_;
+    AVDictionary* opts_;
 
-  QTimer clear_timer_;
+    QTimer clear_timer_;
 
-  QAtomicInt allow_clear_event_;
+    QAtomicInt allow_clear_event_;
 
 private slots:
-  void FreeMemory();
+    void FreeMemory();
 
-  void ClearTimerEvent();
+    void ClearTimerEvent();
 
-  void RestartClearTimer();
+    void RestartClearTimer();
 
 };
 
