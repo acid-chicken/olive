375c7280ff273b8e48b0dc38513bea23cca2b5e8
Restyled.io <commits@restyled.io>
2020-02-21 13:48:54 +0000

    Restyled by clang-format

--- app/render/backend/opengl/openglcolorprocessor.cpp
+++ app/render/backend/opengl/openglcolorprocessor.cpp
@@ -5,73 +5,56 @@
 
 #include "openglrenderfunctions.h"
 
-void OpenGLColorProcessor::Enable(QOpenGLContext *context, bool alpha_is_associated)
-{
-    if (IsEnabled()) {
-        return;
-    }
+void OpenGLColorProcessor::Enable(QOpenGLContext *context, bool alpha_is_associated) {
+  if (IsEnabled()) {
+    return;
+  }
 
-    context_ = context;
+  context_ = context;
 
-    pipeline_ = OpenGLShader::CreateOCIO(context_,
-                                         ocio_lut_,
-                                         GetProcessor(),
-                                         alpha_is_associated);
+  pipeline_ = OpenGLShader::CreateOCIO(context_, ocio_lut_, GetProcessor(), alpha_is_associated);
 
-    connect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture, Qt::DirectConnection);
+  connect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture,
+          Qt::DirectConnection);
 }
 
-bool OpenGLColorProcessor::IsEnabled() const
-{
-    return ocio_lut_;
+bool OpenGLColorProcessor::IsEnabled() const { return ocio_lut_; }
+
+OpenGLShaderPtr OpenGLColorProcessor::pipeline() const { return pipeline_; }
+
+void OpenGLColorProcessor::ProcessOpenGL(bool flipped, const QMatrix4x4 &matrix) {
+  OpenGLRenderFunctions::OCIOBlit(pipeline_, ocio_lut_, flipped, matrix);
 }
 
-OpenGLShaderPtr OpenGLColorProcessor::pipeline() const
-{
-    return pipeline_;
+void OpenGLColorProcessor::ClearTexture() {
+  if (IsEnabled()) {
+    // Clean up OCIO LUT texture and shader
+    context_->functions()->glDeleteTextures(1, &ocio_lut_);
+
+    disconnect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture);
+
+    ocio_lut_ = 0;
+    pipeline_ = nullptr;
+  }
 }
 
-void OpenGLColorProcessor::ProcessOpenGL(bool flipped, const QMatrix4x4& matrix)
-{
-    OpenGLRenderFunctions::OCIOBlit(pipeline_, ocio_lut_, flipped, matrix);
+OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space,
+                                           const QString &dest_space)
+    : ColorProcessor(config, source_space, dest_space), ocio_lut_(0) {}
+
+OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space, QString display,
+                                           QString view, const QString &look)
+    : ColorProcessor(config, source_space, display, view, look), ocio_lut_(0) {}
+
+OpenGLColorProcessor::~OpenGLColorProcessor() { ClearTexture(); }
+
+OpenGLColorProcessorPtr OpenGLColorProcessor::CreateOpenGL(OCIO::ConstConfigRcPtr config, const QString &source_space,
+                                                           const QString &dest_space) {
+  return std::make_shared<OpenGLColorProcessor>(config, source_space, dest_space);
 }
 
-void OpenGLColorProcessor::ClearTexture()
-{
-    if (IsEnabled()) {
-        // Clean up OCIO LUT texture and shader
-        context_->functions()->glDeleteTextures(1, &ocio_lut_);
-
-        disconnect(context_, &QOpenGLContext::aboutToBeDestroyed, this, &OpenGLColorProcessor::ClearTexture);
-
-        ocio_lut_ = 0;
-        pipeline_ = nullptr;
-    }
-}
-
-OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &dest_space) :
-    ColorProcessor(config, source_space, dest_space),
-    ocio_lut_(0)
-{
-}
-
-OpenGLColorProcessor::OpenGLColorProcessor(OCIO::ConstConfigRcPtr config, const QString &source_space, QString display, QString view, const QString &look) :
-    ColorProcessor(config, source_space, display, view, look),
-    ocio_lut_(0)
-{
-}
-
-OpenGLColorProcessor::~OpenGLColorProcessor()
-{
-    ClearTexture();
-}
-
-OpenGLColorProcessorPtr OpenGLColorProcessor::CreateOpenGL(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &dest_space)
-{
-    return std::make_shared<OpenGLColorProcessor>(config, source_space, dest_space);
-}
-
-OpenGLColorProcessorPtr OpenGLColorProcessor::CreateOpenGL(OCIO::ConstConfigRcPtr config, const QString &source_space, const QString &display, const QString &view, const QString &look)
-{
-    return std::make_shared<OpenGLColorProcessor>(config, source_space, display, view, look);
+OpenGLColorProcessorPtr OpenGLColorProcessor::CreateOpenGL(OCIO::ConstConfigRcPtr config, const QString &source_space,
+                                                           const QString &display, const QString &view,
+                                                           const QString &look) {
+  return std::make_shared<OpenGLColorProcessor>(config, source_space, display, view, look);
 }
--- app/widget/viewer/viewerglwidget.cpp
+++ app/widget/viewer/viewerglwidget.cpp
@@ -35,273 +35,225 @@
 bool ViewerGLWidget::nouveau_check_done_ = false;
 #endif
 
-ViewerGLWidget::ViewerGLWidget(QWidget *parent) :
-    QOpenGLWidget(parent),
-    color_manager_(nullptr),
-    has_image_(false)
-{
-    setContextMenuPolicy(Qt::CustomContextMenu);
+ViewerGLWidget::ViewerGLWidget(QWidget *parent) : QOpenGLWidget(parent), color_manager_(nullptr), has_image_(false) {
+  setContextMenuPolicy(Qt::CustomContextMenu);
 }
 
-ViewerGLWidget::~ViewerGLWidget()
-{
-    ContextCleanup();
+ViewerGLWidget::~ViewerGLWidget() { ContextCleanup(); }
+
+void ViewerGLWidget::ConnectColorManager(ColorManager *color_manager) {
+  if (color_manager_ != nullptr) {
+    disconnect(color_manager_, SIGNAL(ConfigChanged()), this, SLOT(RefreshColorPipeline()));
+  }
+
+  color_manager_ = color_manager;
+
+  if (color_manager_ != nullptr) {
+    connect(color_manager_, SIGNAL(ConfigChanged()), this, SLOT(RefreshColorPipeline()));
+  }
+
+  RefreshColorPipeline();
 }
 
-void ViewerGLWidget::ConnectColorManager(ColorManager *color_manager)
-{
-    if (color_manager_ != nullptr) {
-        disconnect(color_manager_, SIGNAL(ConfigChanged()), this, SLOT(RefreshColorPipeline()));
-    }
+void ViewerGLWidget::DisconnectColorManager() { ConnectColorManager(nullptr); }
 
-    color_manager_ = color_manager;
-
-    if (color_manager_ != nullptr) {
-        connect(color_manager_, SIGNAL(ConfigChanged()), this, SLOT(RefreshColorPipeline()));
-    }
-
-    RefreshColorPipeline();
+void ViewerGLWidget::SetMatrix(const QMatrix4x4 &mat) {
+  matrix_ = mat;
+  update();
 }
 
-void ViewerGLWidget::DisconnectColorManager()
-{
-    ConnectColorManager(nullptr);
-}
+void ViewerGLWidget::SetImage(const QString &fn) {
+  has_image_ = false;
 
-void ViewerGLWidget::SetMatrix(const QMatrix4x4 &mat)
-{
-    matrix_ = mat;
-    update();
-}
+  if (!fn.isEmpty() && QFile::exists(fn)) {
+    auto input = OIIO::ImageInput::open(fn.toStdString());
 
-void ViewerGLWidget::SetImage(const QString &fn)
-{
-    has_image_ = false;
+    if (input) {
+      PixelFormat::Format image_format = PixelService::OIIOFormatToOliveFormat(input->spec().format);
 
-    if (!fn.isEmpty() && QFile::exists(fn)) {
-        auto input = OIIO::ImageInput::open(fn.toStdString());
+      // Ensure the following texture operations are done in our context (in case we're in a separate window for
+      // instance)
+      makeCurrent();
 
-        if (input) {
+      if (!texture_.IsCreated() || texture_.width() != input->spec().width ||
+          texture_.height() != input->spec().height || texture_.format() != image_format) {
+        load_buffer_.destroy();
+        texture_.Destroy();
 
-            PixelFormat::Format image_format = PixelService::OIIOFormatToOliveFormat(input->spec().format);
+        load_buffer_.set_width(input->spec().width);
+        load_buffer_.set_height(input->spec().height);
+        load_buffer_.set_format(image_format);
+        load_buffer_.allocate();
 
-            // Ensure the following texture operations are done in our context (in case we're in a separate window for instance)
-            makeCurrent();
+        texture_.Create(context(), input->spec().width, input->spec().height, image_format);
+      }
 
-            if (!texture_.IsCreated()
-                    || texture_.width() != input->spec().width
-                    || texture_.height() != input->spec().height
-                    || texture_.format() != image_format) {
-                load_buffer_.destroy();
-                texture_.Destroy();
+      input->read_image(input->spec().format, load_buffer_.data());
+      input->close();
 
-                load_buffer_.set_width(input->spec().width);
-                load_buffer_.set_height(input->spec().height);
-                load_buffer_.set_format(image_format);
-                load_buffer_.allocate();
+      texture_.Upload(load_buffer_.data());
 
-                texture_.Create(context(), input->spec().width, input->spec().height, image_format);
-            }
+      doneCurrent();
 
-            input->read_image(input->spec().format, load_buffer_.data());
-            input->close();
-
-            texture_.Upload(load_buffer_.data());
-
-            doneCurrent();
-
-            has_image_ = true;
+      has_image_ = true;
 
 #if OIIO_VERSION < 10903
-            OIIO::ImageInput::destroy(input);
+      OIIO::ImageInput::destroy(input);
 #endif
-
-        } else {
-            qWarning() << "OIIO Error:" << OIIO::geterror().c_str();
-        }
-    }
-
-    update();
-}
-
-void ViewerGLWidget::SetOCIODisplay(const QString &display)
-{
-    ocio_display_ = display;
-    SetupColorProcessor();
-    update();
-}
-
-void ViewerGLWidget::SetOCIOView(const QString &view)
-{
-    ocio_view_ = view;
-    SetupColorProcessor();
-    update();
-}
-
-void ViewerGLWidget::SetOCIOLook(const QString &look)
-{
-    ocio_look_ = look;
-    SetupColorProcessor();
-    update();
-}
-
-ColorManager *ViewerGLWidget::color_manager() const
-{
-    return color_manager_;
-}
-
-const QString &ViewerGLWidget::ocio_display() const
-{
-    return ocio_display_;
-}
-
-const QString &ViewerGLWidget::ocio_view() const
-{
-    return ocio_view_;
-}
-
-const QString &ViewerGLWidget::ocio_look() const
-{
-    return ocio_look_;
-}
-
-void ViewerGLWidget::SetOCIOParameters(const QString &display, const QString &view, const QString &look)
-{
-    ocio_display_ = display;
-    ocio_view_ = view;
-    ocio_look_ = look;
-    SetupColorProcessor();
-    update();
-}
-
-void ViewerGLWidget::initializeGL()
-{
-    SetupColorProcessor();
-
-    connect(context(), SIGNAL(aboutToBeDestroyed()), this, SLOT(ContextCleanup()), Qt::DirectConnection);
-
-#ifdef Q_OS_LINUX
-    if (!nouveau_check_done_) {
-        const char* vendor = reinterpret_cast<const char*>(context()->functions()->glGetString(GL_VENDOR));
-
-        if (!strcmp(vendor, "nouveau")) {
-            // Working with Qt widgets in this function segfaults, so we queue the messagebox for later
-            QMetaObject::invokeMethod(this,
-                                      "ShowNouveauWarning",
-                                      Qt::QueuedConnection);
-        }
-
-        nouveau_check_done_ = true;
-    }
-#endif
-}
-
-void ViewerGLWidget::paintGL()
-{
-    // Get functions attached to this context (they will already be initialized)
-    QOpenGLFunctions* f = context()->functions();
-
-    // Clear background to empty
-    f->glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    f->glClear(GL_COLOR_BUFFER_BIT);
-
-    // We only draw if we have a pipeline
-    if (!has_image_ || !color_service_ || !texture_.IsCreated()) {
-        return;
-    }
-
-    // Bind retrieved texture
-    f->glBindTexture(GL_TEXTURE_2D, texture_.texture());
-
-    // Blit using the color service
-    color_service_->ProcessOpenGL(true, matrix_);
-
-    // Release retrieved texture
-    f->glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void ViewerGLWidget::RefreshColorPipeline()
-{
-    if (!color_manager_) {
-        color_service_ = nullptr;
-        return;
-    }
-
-    QStringList displays = color_manager_->ListAvailableDisplays();
-    if (!displays.contains(ocio_display_)) {
-        ocio_display_ = color_manager_->GetDefaultDisplay();
-    }
-
-    QStringList views = color_manager_->ListAvailableViews(ocio_display_);
-    if (!views.contains(ocio_view_)) {
-        ocio_view_ = color_manager_->GetDefaultView(ocio_display_);
-    }
-
-    QStringList looks = color_manager_->ListAvailableLooks();
-    if (!looks.contains(ocio_look_)) {
-        ocio_look_.clear();
-    }
-
-    SetupColorProcessor();
-    update();
-}
-
-#ifdef Q_OS_LINUX
-void ViewerGLWidget::ShowNouveauWarning()
-{
-    QMessageBox::warning(this,
-                         tr("Driver Warning"),
-                         tr("Olive has detected your system is using the Nouveau graphics driver.\n\nThis driver is "
-                            "known to have stability and performance issues with Olive. It is highly recommended "
-                            "you install the proprietary NVIDIA driver before continuing to use Olive."),
-                         QMessageBox::Ok);
-}
-#endif
-
-void ViewerGLWidget::SetupColorProcessor()
-{
-    if (!context()) {
-        return;
-    }
-
-    color_service_ = nullptr;
-
-    if (color_manager_) {
-        // (Re)create color processor
-
-        try {
-            ColorProcessorPtr new_service = ColorProcessor::Create(color_manager_->GetConfig(),
-                                            OCIO::ROLE_SCENE_LINEAR,
-                                            ocio_display_,
-                                            ocio_view_,
-                                            ocio_look_);
-
-            color_service_ = OpenGLColorProcessor::CreateOpenGL(color_manager_->GetConfig(),
-                             OCIO::ROLE_SCENE_LINEAR,
-                             ocio_display_,
-                             ocio_view_,
-                             ocio_look_);
-
-            color_service_->Enable(context(), true);
-
-        } catch (OCIO::Exception& e) {
-            QMessageBox::critical(this,
-                                  tr("OpenColorIO Error"),
-                                  tr("Failed to set color configuration: %1").arg(e.what()),
-                                  QMessageBox::Ok);
-        }
 
     } else {
-        color_service_ = nullptr;
+      qWarning() << "OIIO Error:" << OIIO::geterror().c_str();
     }
+  }
+
+  update();
 }
 
-void ViewerGLWidget::ContextCleanup()
-{
-    makeCurrent();
+void ViewerGLWidget::SetOCIODisplay(const QString &display) {
+  ocio_display_ = display;
+  SetupColorProcessor();
+  update();
+}
 
+void ViewerGLWidget::SetOCIOView(const QString &view) {
+  ocio_view_ = view;
+  SetupColorProcessor();
+  update();
+}
+
+void ViewerGLWidget::SetOCIOLook(const QString &look) {
+  ocio_look_ = look;
+  SetupColorProcessor();
+  update();
+}
+
+ColorManager *ViewerGLWidget::color_manager() const { return color_manager_; }
+
+const QString &ViewerGLWidget::ocio_display() const { return ocio_display_; }
+
+const QString &ViewerGLWidget::ocio_view() const { return ocio_view_; }
+
+const QString &ViewerGLWidget::ocio_look() const { return ocio_look_; }
+
+void ViewerGLWidget::SetOCIOParameters(const QString &display, const QString &view, const QString &look) {
+  ocio_display_ = display;
+  ocio_view_ = view;
+  ocio_look_ = look;
+  SetupColorProcessor();
+  update();
+}
+
+void ViewerGLWidget::initializeGL() {
+  SetupColorProcessor();
+
+  connect(context(), SIGNAL(aboutToBeDestroyed()), this, SLOT(ContextCleanup()), Qt::DirectConnection);
+
+#ifdef Q_OS_LINUX
+  if (!nouveau_check_done_) {
+    const char *vendor = reinterpret_cast<const char *>(context()->functions()->glGetString(GL_VENDOR));
+
+    if (!strcmp(vendor, "nouveau")) {
+      // Working with Qt widgets in this function segfaults, so we queue the messagebox for later
+      QMetaObject::invokeMethod(this, "ShowNouveauWarning", Qt::QueuedConnection);
+    }
+
+    nouveau_check_done_ = true;
+  }
+#endif
+}
+
+void ViewerGLWidget::paintGL() {
+  // Get functions attached to this context (they will already be initialized)
+  QOpenGLFunctions *f = context()->functions();
+
+  // Clear background to empty
+  f->glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  f->glClear(GL_COLOR_BUFFER_BIT);
+
+  // We only draw if we have a pipeline
+  if (!has_image_ || !color_service_ || !texture_.IsCreated()) {
+    return;
+  }
+
+  // Bind retrieved texture
+  f->glBindTexture(GL_TEXTURE_2D, texture_.texture());
+
+  // Blit using the color service
+  color_service_->ProcessOpenGL(true, matrix_);
+
+  // Release retrieved texture
+  f->glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void ViewerGLWidget::RefreshColorPipeline() {
+  if (!color_manager_) {
     color_service_ = nullptr;
-    texture_.Destroy();
+    return;
+  }
 
-    doneCurrent();
+  QStringList displays = color_manager_->ListAvailableDisplays();
+  if (!displays.contains(ocio_display_)) {
+    ocio_display_ = color_manager_->GetDefaultDisplay();
+  }
+
+  QStringList views = color_manager_->ListAvailableViews(ocio_display_);
+  if (!views.contains(ocio_view_)) {
+    ocio_view_ = color_manager_->GetDefaultView(ocio_display_);
+  }
+
+  QStringList looks = color_manager_->ListAvailableLooks();
+  if (!looks.contains(ocio_look_)) {
+    ocio_look_.clear();
+  }
+
+  SetupColorProcessor();
+  update();
+}
+
+#ifdef Q_OS_LINUX
+void ViewerGLWidget::ShowNouveauWarning() {
+  QMessageBox::warning(this, tr("Driver Warning"),
+                       tr("Olive has detected your system is using the Nouveau graphics driver.\n\nThis driver is "
+                          "known to have stability and performance issues with Olive. It is highly recommended "
+                          "you install the proprietary NVIDIA driver before continuing to use Olive."),
+                       QMessageBox::Ok);
+}
+#endif
+
+void ViewerGLWidget::SetupColorProcessor() {
+  if (!context()) {
+    return;
+  }
+
+  color_service_ = nullptr;
+
+  if (color_manager_) {
+    // (Re)create color processor
+
+    try {
+      ColorProcessorPtr new_service = ColorProcessor::Create(color_manager_->GetConfig(), OCIO::ROLE_SCENE_LINEAR,
+                                                             ocio_display_, ocio_view_, ocio_look_);
+
+      color_service_ = OpenGLColorProcessor::CreateOpenGL(color_manager_->GetConfig(), OCIO::ROLE_SCENE_LINEAR,
+                                                          ocio_display_, ocio_view_, ocio_look_);
+
+      color_service_->Enable(context(), true);
+
+    } catch (OCIO::Exception &e) {
+      QMessageBox::critical(this, tr("OpenColorIO Error"), tr("Failed to set color configuration: %1").arg(e.what()),
+                            QMessageBox::Ok);
+    }
+
+  } else {
+    color_service_ = nullptr;
+  }
+}
+
+void ViewerGLWidget::ContextCleanup() {
+  makeCurrent();
+
+  color_service_ = nullptr;
+  texture_.Destroy();
+
+  doneCurrent();
 }
