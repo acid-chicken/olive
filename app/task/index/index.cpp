c726653de495c2cc5f3b4bfa23b510975826441b
Restyled.io <commits@restyled.io>
2020-02-21 09:48:48 +0000

    Restyled by clang-format

--- app/task/index/index.cpp
+++ app/task/index/index.cpp
@@ -3,27 +3,24 @@
 #include "codec/decoder.h"
 #include "codec/ffmpeg/ffmpegdecoder.h"
 
-IndexTask::IndexTask(StreamPtr stream) :
-    stream_(stream)
-{
-    SetTitle(tr("Indexing %1:%2").arg(stream_->footage()->filename(), QString::number(stream_->index())));
+IndexTask::IndexTask(StreamPtr stream) : stream_(stream) {
+  SetTitle(tr("Indexing %1:%2").arg(stream_->footage()->filename(), QString::number(stream_->index())));
 }
 
-void IndexTask::Action()
-{
-    if (stream_->footage()->decoder().isEmpty()) {
-        emit Failed(QStringLiteral("Stream has no decoder"));
-    } else {
-        DecoderPtr decoder = Decoder::CreateFromID(stream_->footage()->decoder());
+void IndexTask::Action() {
+  if (stream_->footage()->decoder().isEmpty()) {
+    emit Failed(QStringLiteral("Stream has no decoder"));
+  } else {
+    DecoderPtr decoder = Decoder::CreateFromID(stream_->footage()->decoder());
 
-        decoder->set_stream(stream_);
+    decoder->set_stream(stream_);
 
-        connect(decoder.get(), &Decoder::IndexProgress, this, &IndexTask::ProgressChanged);
+    connect(decoder.get(), &Decoder::IndexProgress, this, &IndexTask::ProgressChanged);
 
-        decoder->Open();
-        decoder->Index(&IsCancelled());
-        decoder->Close();
+    decoder->Open();
+    decoder->Index(&IsCancelled());
+    decoder->Close();
 
-        emit Succeeded();
-    }
+    emit Succeeded();
+  }
 }
