46934890c8333ebab4b8b5de1cb3837a79ebdf01
Restyled.io <commits@restyled.io>
2020-02-23 15:48:48 +0000

    Restyled by astyle

--- app/codec/ffmpeg/ffmpegframecache.cpp
+++ app/codec/ffmpeg/ffmpegframecache.cpp
@@ -8,99 +8,99 @@ QList<Frame*> FFmpegFrameCache::frame_pool_;
 
 Frame *FFmpegFrameCache::Client::append(const VideoRenderingParams& params)
 {
-  Frame* f = FFmpegFrameCache::Get(params);
+    Frame* f = FFmpegFrameCache::Get(params);
 
-  frames_.append({f, QDateTime::currentMSecsSinceEpoch()});
+    frames_.append({f, QDateTime::currentMSecsSinceEpoch()});
 
-  return f;
+    return f;
 }
 
 void FFmpegFrameCache::Client::clear()
 {
-  foreach (const CachedFrame& cf, frames_) {
-    FFmpegFrameCache::Release(cf.frame);
-  }
-  frames_.clear();
+    foreach (const CachedFrame& cf, frames_) {
+        FFmpegFrameCache::Release(cf.frame);
+    }
+    frames_.clear();
 }
 
 bool FFmpegFrameCache::Client::isEmpty() const
 {
-  return frames_.isEmpty();
+    return frames_.isEmpty();
 }
 
 Frame *FFmpegFrameCache::Client::first() const
 {
-  return frames_.first().frame;
+    return frames_.first().frame;
 }
 
 Frame *FFmpegFrameCache::Client::at(int i) const
 {
-  return frames_.at(i).frame;
+    return frames_.at(i).frame;
 }
 
 Frame *FFmpegFrameCache::Client::last() const
 {
-  return frames_.last().frame;
+    return frames_.last().frame;
 }
 
 int FFmpegFrameCache::Client::size() const
 {
-  return frames_.size();
+    return frames_.size();
 }
 
 void FFmpegFrameCache::Client::accessedFirst()
 {
-  frames_.first().accessed = QDateTime::currentMSecsSinceEpoch();
+    frames_.first().accessed = QDateTime::currentMSecsSinceEpoch();
 }
 
 void FFmpegFrameCache::Client::accessedLast()
 {
-  frames_.last().accessed = QDateTime::currentMSecsSinceEpoch();
+    frames_.last().accessed = QDateTime::currentMSecsSinceEpoch();
 }
 
 void FFmpegFrameCache::Client::accessed(int i)
 {
-  frames_[i].accessed = QDateTime::currentMSecsSinceEpoch();
+    frames_[i].accessed = QDateTime::currentMSecsSinceEpoch();
 }
 
 void FFmpegFrameCache::Client::remove_old_frames(qint64 older_than)
 {
-  //int counter = 0;
+    //int counter = 0;
 
-  while (!frames_.isEmpty() && frames_.first().accessed < older_than) {
-    FFmpegFrameCache::Release(frames_.takeFirst().frame);
-    //counter++;
-  }
+    while (!frames_.isEmpty() && frames_.first().accessed < older_than) {
+        FFmpegFrameCache::Release(frames_.takeFirst().frame);
+        //counter++;
+    }
 
-  //qDebug() << "  * Removed" << counter << "frames";
+    //qDebug() << "  * Removed" << counter << "frames";
 }
 
 Frame* FFmpegFrameCache::Get(const VideoRenderingParams &params)
 {
-  QMutexLocker locker(&pool_lock_);
+    QMutexLocker locker(&pool_lock_);
 
-  // See if we have a frame matching this description in the pool
-  for (int i=0;i<frame_pool_.size();i++) {
-    if (frame_pool_.at(i)->width() == params.width()
-        && frame_pool_.at(i)->height() == params.height()
-        && frame_pool_.at(i)->format() == params.format()) {
-      return frame_pool_.takeAt(i);
+    // See if we have a frame matching this description in the pool
+    for (int i=0; i<frame_pool_.size(); i++) {
+        if (frame_pool_.at(i)->width() == params.width()
+                && frame_pool_.at(i)->height() == params.height()
+                && frame_pool_.at(i)->format() == params.format()) {
+            return frame_pool_.takeAt(i);
+        }
     }
-  }
 
-  // Otherwise we'll need to create one
-  Frame* f = new Frame();
-  f->set_width(params.width());
-  f->set_height(params.height());
-  f->set_format(params.format());
-  f->allocate();
+    // Otherwise we'll need to create one
+    Frame* f = new Frame();
+    f->set_width(params.width());
+    f->set_height(params.height());
+    f->set_format(params.format());
+    f->allocate();
 
-  return f;
+    return f;
 }
 
 void FFmpegFrameCache::Release(Frame *f)
 {
-  QMutexLocker locker(&pool_lock_);
+    QMutexLocker locker(&pool_lock_);
 
-  frame_pool_.append(f);
+    frame_pool_.append(f);
 }
--- app/render/backend/audiorenderworker.cpp
+++ app/render/backend/audiorenderworker.cpp
@@ -3,107 +3,107 @@
 #include "audio/audiomanager.h"
 
 AudioRenderWorker::AudioRenderWorker(DecoderCache* decoder_cache, QObject *parent) :
-  RenderWorker(decoder_cache, parent)
+    RenderWorker(decoder_cache, parent)
 {
 }
 
 void AudioRenderWorker::SetParameters(const AudioRenderingParams &audio_params)
 {
-  audio_params_ = audio_params;
+    audio_params_ = audio_params;
 }
 
 bool AudioRenderWorker::InitInternal()
 {
-  // Nothing to init yet
-  return true;
+    // Nothing to init yet
+    return true;
 }
 
 void AudioRenderWorker::CloseInternal()
 {
-  // Nothing to init yet
+    // Nothing to init yet
 }
 
 FramePtr AudioRenderWorker::RetrieveFromDecoder(DecoderPtr decoder, const TimeRange &range)
 {
-  if (decoder->HasConformedVersion(audio_params_)) {
-    return decoder->RetrieveAudio(range.in(), range.out() - range.in(), audio_params_);
-  } else {
-    emit ConformUnavailable(decoder->stream(), CurrentPath().range(), range.out(), audio_params_);
-    return nullptr;
-  }
+    if (decoder->HasConformedVersion(audio_params_)) {
+        return decoder->RetrieveAudio(range.in(), range.out() - range.in(), audio_params_);
+    } else {
+        emit ConformUnavailable(decoder->stream(), CurrentPath().range(), range.out(), audio_params_);
+        return nullptr;
+    }
 }
 
 NodeValueTable AudioRenderWorker::RenderBlock(const TrackOutput *track, const TimeRange &range)
 {
-  QList<Block*> active_blocks = track->BlocksAtTimeRange(range);
+    QList<Block*> active_blocks = track->BlocksAtTimeRange(range);
 
-  // All these blocks will need to output to a buffer so we create one here
-  QByteArray block_range_buffer(audio_params_.time_to_bytes(range.length()), 0);
+    // All these blocks will need to output to a buffer so we create one here
+    QByteArray block_range_buffer(audio_params_.time_to_bytes(range.length()), 0);
 
-  NodeValueTable merged_table;
+    NodeValueTable merged_table;
 
-  // Loop through active blocks retrieving their audio
-  foreach (Block* b, active_blocks) {
-    TimeRange range_for_block(qMax(b->in(), range.in()),
-                              qMin(b->out(), range.out()));
+    // Loop through active blocks retrieving their audio
+    foreach (Block* b, active_blocks) {
+        TimeRange range_for_block(qMax(b->in(), range.in()),
+                                  qMin(b->out(), range.out()));
 
-    // Destination buffer
-    NodeValueTable table = ProcessNode(NodeDependency(b, range_for_block));
-    QByteArray samples_from_this_block = table.Take(NodeParam::kSamples).toByteArray();
+        // Destination buffer
+        NodeValueTable table = ProcessNode(NodeDependency(b, range_for_block));
+        QByteArray samples_from_this_block = table.Take(NodeParam::kSamples).toByteArray();
 
-    if (!samples_from_this_block.isEmpty()) {
-      // Stretch samples here
-      rational abs_speed = qAbs(b->speed());
+        if (!samples_from_this_block.isEmpty()) {
+            // Stretch samples here
+            rational abs_speed = qAbs(b->speed());
 
-      if (abs_speed != 1) {
-        QByteArray speed_adjusted_samples;
+            if (abs_speed != 1) {
+                QByteArray speed_adjusted_samples;
 
-        double clip_speed = abs_speed.toDouble();
+                double clip_speed = abs_speed.toDouble();
 
-        int sample_count = audio_params_.bytes_to_samples(samples_from_this_block.size());
+                int sample_count = audio_params_.bytes_to_samples(samples_from_this_block.size());
 
-        for (double i=0;i<sample_count;i+=clip_speed) {
-          int sample_index = qFloor(i);
-          int byte_index = audio_params_.samples_to_bytes(sample_index);
+                for (double i=0; i<sample_count; i+=clip_speed) {
+                    int sample_index = qFloor(i);
+                    int byte_index = audio_params_.samples_to_bytes(sample_index);
 
-          QByteArray sample_at_this_index = samples_from_this_block.mid(byte_index, audio_params_.samples_to_bytes(1));
-          speed_adjusted_samples.append(sample_at_this_index);
+                    QByteArray sample_at_this_index = samples_from_this_block.mid(byte_index, audio_params_.samples_to_bytes(1));
+                    speed_adjusted_samples.append(sample_at_this_index);
+                }
+
+                samples_from_this_block = speed_adjusted_samples;
+            }
+
+            if (b->is_reversed()) {
+                // Reverse the audio buffer
+                AudioManager::ReverseBuffer(samples_from_this_block.data(), samples_from_this_block.size(), audio_params_.samples_to_bytes(1));
+            }
+
+            int destination_offset = audio_params_.time_to_bytes(range_for_block.in() - range.in());
+            int actual_copy_size = qMin(audio_params_.time_to_bytes(range_for_block.length()), samples_from_this_block.size());
+
+            if (destination_offset < 0
+                    || destination_offset + actual_copy_size > block_range_buffer.size()) {
+                qCritical() << "Tried to copy audio beyond the size of the destination buffer";
+                qCritical() << "  Destination size:" << block_range_buffer.size();
+                qCritical() << "  Destination offset:" << destination_offset;
+                qCritical() << "  Copy size:" << actual_copy_size;
+                abort();
+            } else {
+                memcpy(block_range_buffer.data()+destination_offset,
+                       samples_from_this_block.data(),
+                       actual_copy_size);
+            }
         }
 
-        samples_from_this_block = speed_adjusted_samples;
-      }
-
-      if (b->is_reversed()) {
-        // Reverse the audio buffer
-        AudioManager::ReverseBuffer(samples_from_this_block.data(), samples_from_this_block.size(), audio_params_.samples_to_bytes(1));
-      }
-
-      int destination_offset = audio_params_.time_to_bytes(range_for_block.in() - range.in());
-      int actual_copy_size = qMin(audio_params_.time_to_bytes(range_for_block.length()), samples_from_this_block.size());
-
-      if (destination_offset < 0
-          || destination_offset + actual_copy_size > block_range_buffer.size()) {
-        qCritical() << "Tried to copy audio beyond the size of the destination buffer";
-        qCritical() << "  Destination size:" << block_range_buffer.size();
-        qCritical() << "  Destination offset:" << destination_offset;
-        qCritical() << "  Copy size:" << actual_copy_size;
-        abort();
-      } else {
-        memcpy(block_range_buffer.data()+destination_offset,
-               samples_from_this_block.data(),
-               actual_copy_size);
-      }
+        NodeValueTable::Merge({merged_table, table});
     }
 
-    NodeValueTable::Merge({merged_table, table});
-  }
+    merged_table.Push(NodeParam::kSamples, block_range_buffer);
 
-  merged_table.Push(NodeParam::kSamples, block_range_buffer);
-
-  return merged_table;
+    return merged_table;
 }
 
 const AudioRenderingParams &AudioRenderWorker::audio_params() const
 {
-  return audio_params_;
+    return audio_params_;
 }
