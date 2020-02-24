495fff459e61e7c09db6c23ac978e9f483ac03e4
Restyled.io <commits@restyled.io>
2020-02-24 01:48:49 +0000

    Restyled by astyle

--- app/render/videoparams.h
+++ app/render/videoparams.h
@@ -8,46 +8,46 @@
 class VideoParams
 {
 public:
-  VideoParams();
-  VideoParams(const int& width, const int& height, const rational& time_base);
+    VideoParams();
+    VideoParams(const int& width, const int& height, const rational& time_base);
 
-  const int& width() const;
-  const int& height() const;
-  const rational& time_base() const;
+    const int& width() const;
+    const int& height() const;
+    const rational& time_base() const;
 
 private:
-  int width_;
-  int height_;
-  rational time_base_;
+    int width_;
+    int height_;
+    rational time_base_;
 
 };
 
 class VideoRenderingParams : public VideoParams {
 public:
-  VideoRenderingParams();
-  VideoRenderingParams(const int& width, const int& height, const rational& time_base, const PixelFormat::Format& format, const RenderMode::Mode& mode, const int& divider = 1);
-  VideoRenderingParams(const VideoParams& params, const PixelFormat::Format& format, const RenderMode::Mode& mode, const int& divider = 1);
+    VideoRenderingParams();
+    VideoRenderingParams(const int& width, const int& height, const rational& time_base, const PixelFormat::Format& format, const RenderMode::Mode& mode, const int& divider = 1);
+    VideoRenderingParams(const VideoParams& params, const PixelFormat::Format& format, const RenderMode::Mode& mode, const int& divider = 1);
 
-  const int& divider() const;
-  const int& effective_width() const;
-  const int& effective_height() const;
+    const int& divider() const;
+    const int& effective_width() const;
+    const int& effective_height() const;
 
-  bool is_valid() const;
-  const PixelFormat::Format& format() const;
-  const RenderMode::Mode& mode() const;
+    bool is_valid() const;
+    const PixelFormat::Format& format() const;
+    const RenderMode::Mode& mode() const;
 
-  bool operator==(const VideoRenderingParams& rhs) const;
-  bool operator!=(const VideoRenderingParams& rhs) const;
+    bool operator==(const VideoRenderingParams& rhs) const;
+    bool operator!=(const VideoRenderingParams& rhs) const;
 
 private:
-  void calculate_effective_size();
+    void calculate_effective_size();
 
-  PixelFormat::Format format_;
-  RenderMode::Mode mode_;
+    PixelFormat::Format format_;
+    RenderMode::Mode mode_;
 
-  int divider_;
-  int effective_width_;
-  int effective_height_;
+    int divider_;
+    int effective_width_;
+    int effective_height_;
 };
 
 #endif // VIDEOPARAMS_H
