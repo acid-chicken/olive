56aae2d46150d23d749c6cace42817a0eac2e5d3
Restyled.io <commits@restyled.io>
2020-04-02 01:59:31 +0000

    Restyled by astyle

--- app/render/color.cpp
+++ app/render/color.cpp
@@ -2,13 +2,13 @@
 
 Color::Color(const char *data, const PixelFormat::Format &format)
 {
-  OIIO::convert_types(PixelFormat::GetOIIOTypeDesc(format),
-                      data,
-                      PixelFormat::GetOIIOTypeDesc(PixelFormat::PIX_FMT_RGB32F),
-                      data_,
-                      PixelFormat::FormatHasAlphaChannel(format) ? kRGBAChannels : kRGBChannels);
+    OIIO::convert_types(PixelFormat::GetOIIOTypeDesc(format),
+                        data,
+                        PixelFormat::GetOIIOTypeDesc(PixelFormat::PIX_FMT_RGB32F),
+                        data_,
+                        PixelFormat::FormatHasAlphaChannel(format) ? kRGBAChannels : kRGBChannels);
 
-  if (!PixelFormat::FormatHasAlphaChannel(format)) {
-    set_alpha(1.0f);
-  }
+    if (!PixelFormat::FormatHasAlphaChannel(format)) {
+        set_alpha(1.0f);
+    }
 }
