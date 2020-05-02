9fcc42e13c2e54df757252ea834781aa26a56764
Restyled.io <commits@restyled.io>
2020-05-02 16:29:52 +0000

    Restyled by shellharden

--- app/render/ocioconf/gen-qrc.sh
+++ app/render/ocioconf/gen-qrc.sh
@@ -5,7 +5,7 @@ rm ocioconf.qrc
 echo "<RCC>" >> ocioconf.qrc
 echo "  <qresource prefix=\"/ocioconf\">" >> ocioconf.qrc
 
-for f in $(find * -type f)
+for f in "$(find * -type f)"
 do
     if [ "$f" != "CMakeLists.txt" ] && [ "$f" != "ocioconf.qrc" ] && [ "$f" != "$ourbasename" ]
     then
