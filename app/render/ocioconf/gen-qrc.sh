893329843e903ff64b24723cf6998291821dee60
Restyled.io <commits@restyled.io>
2020-05-02 16:29:53 +0000

    Restyled by shfmt

--- app/render/ocioconf/gen-qrc.sh
+++ app/render/ocioconf/gen-qrc.sh
@@ -2,16 +2,14 @@
 ourbasename=$(basename "$0")
 
 rm ocioconf.qrc
-echo "<RCC>" >> ocioconf.qrc
-echo "  <qresource prefix=\"/ocioconf\">" >> ocioconf.qrc
+echo "<RCC>" >>ocioconf.qrc
+echo "  <qresource prefix=\"/ocioconf\">" >>ocioconf.qrc
 
-for f in "$(find * -type f)"
-do
-    if [ "$f" != "CMakeLists.txt" ] && [ "$f" != "ocioconf.qrc" ] && [ "$f" != "$ourbasename" ]
-    then
-        echo "    <file>$f</file>" >> ocioconf.qrc
-    fi
+for f in "$(find * -type f)"; do
+  if [ "$f" != "CMakeLists.txt" ] && [ "$f" != "ocioconf.qrc" ] && [ "$f" != "$ourbasename" ]; then
+    echo "    <file>$f</file>" >>ocioconf.qrc
+  fi
 done
 
-echo "  </qresource>" >> ocioconf.qrc
-echo "</RCC>" >> ocioconf.qrc
+echo "  </qresource>" >>ocioconf.qrc
+echo "</RCC>" >>ocioconf.qrc
